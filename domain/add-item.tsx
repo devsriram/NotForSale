c7b06b8d230c1337d24d2666a0e6d9a6f2a07dd1
devsriram <sriramdvn@gmail.com>
2021-01-18 02:36:01 -0500

    Add Item to Firebase

--- domain/add-item.tsx
+++ domain/add-item.tsx
@@ -8,8 +8,28 @@ import ImageInput from "../components/image-picker"
 import RadioGroup from "react-native-radio-buttons-group";
 import RadioButtons from "../components/radio-buttons"
 import {PrimaryButton} from "../components/custom-button"
+import * as Yup from "yup";
+import { Formik } from "formik";
+import {initialItemData, coordinates} from "../types"
+import AddItemService from "../firebase-services/add-item-service"
+import itemService from '../firebase-services/add-item-service';
+import addItemService from '../firebase-services/add-item-service';
+import { date } from 'yup/lib/locale';
+import firebase from "../firebase-config"
+
+const validationSchema = Yup.object().shape({
+    itemName: Yup.string()
+        .required("Required*")
+        .min(5, "Minimum 5 characters Needed")
+        .label("Item Name"),
+    description: Yup.string().label("Item Description"),
+  });
 
 function AddItemScreen(props) {
+
+    const [itemId, setItemId] = useState(0);
+
+    const [itemData, setItemData] = useState(initialItemData);
 
     const itemConditions = [
         {
@@ -45,38 +65,126 @@ function AddItemScreen(props) {
         }
     ]
 
+    const addItem = async (values) => {
+        itemData["itemName"] = values.itemName;
+        itemData["desc"] = values.description;
+        navigator.geolocation.getCurrentPosition((position) => {
+            const coordinateData : coordinates[] = [];
+            const data = {
+                longitute : position.coords.longitude,
+                latitude : position.coords.latitude
+            }
+            coordinateData.push(data);
+            itemData["coordinates"] = coordinateData;
+        })
+        itemData["timestamp"] = new Date();
+        let itemNum = await addItemService().getItemId();
+        let newItemId: number = itemNum.data()?.itemId;
+        itemData["itemId"] = newItemId;
+        itemData["userId"] = "sriram";
+        addItemService().setItemId();
+        if(itemData.imageUri != "") {
+            await addItemService().storeImage(itemData.imageUri, itemData.itemId)
+            .then(() => {
+                itemData.imageUri = newItemId.toString();
+            })
+        }
+        //console.log(itemData);
+        addItemService().addItem(itemData, itemData.itemId)
+        .then(() => {
+            alert("Item Added");
+        })
+          .catch((error) => {
+            alert("Item Not added");
+            console.log("error is :",error);
+        })
+        
+    }
+
     return (
         <SafeAreaView style = {styles.safeAreaContainer}>
             <ScrollView style = {styles.scroll}>
             <View style = {styles.container}>
-                <ImageInput />
-                <View style = {styles.gap}></View>
-                <View style = {styles.nameContainer}>
-                    <Text style = {styles.conditionText}>Item Name</Text>
-                    <PrimaryInput 
-                        placeholderText = "Hii"
-                    />
-                </View>
+                <ImageInput data = {itemData}/>
+                <Formik
+                    initialValues = {{
+                        itemName : "",
+                        description : ""
+                    }}
+                    onSubmit={(values) => {
+                        addItem(values);
+                    }}
+                    validationSchema = {validationSchema}
+                >
+                    {({
+                        values,
+                        handleChange,
+                        handleSubmit,
+                        errors,
+                        setFieldTouched,
+                        touched,
+                    }) => (
+                    <>
+                    <View style = {styles.nameContainer}>
+                        <Text style = {styles.conditionText}>Item Name</Text>
+                        <PrimaryInput 
+                            placeholderText = "Hii"
+                            value = {values.itemName}
+                            onChangeText={handleChange("itemName")}
+                            onBlur={() => setFieldTouched("itemName")}
+                        />
+                        <View>
+                            {touched.itemName && (
+                            <Text style={styles.errorText}>{errors.itemName}</Text>
+                            )}
+                        </View>
+                    </View>
                 
-                <View style = {styles.conditionContainer}>
-                    <Text style = {styles.conditionText}>Item Condition</Text>
-                    <RadioButtons data = {itemConditions} />
-                </View>
-
-                <View style = {styles.contactContainer}>
-                    <Text style = {styles.conditionText}>Show Contact</Text>
-                    <RadioButtons data = {contact} />
-                </View>
-                <View style = {styles.contactContainer}>
-                    <Text style = {styles.conditionText}>Description</Text>
-                    <View style = {styles.descContainer}>
-                        <TextInput
-                            style = {styles.descText}
-                            numberOfLines = {3}
+                    <View style = {styles.conditionContainer}>
+                        <Text style = {styles.conditionText}>Item Condition</Text>
+                        <RadioButtons 
+                            data = {itemConditions} 
+                            itemData = {itemData} 
+                            type = "condition"
                         />
                     </View>
-                </View>
-                <PrimaryButton buttonTitle = "Submit" style = {styles.submitButton}/>
+
+                    <View style = {styles.contactContainer}>
+                        <Text style = {styles.conditionText}>Show Contact</Text>
+                        <RadioButtons 
+                            data = {contact} 
+                            itemData = {itemData}
+                            type = "contact"
+                        />
+                    </View>
+                    <View style = {styles.contactContainer}>
+                        <Text style = {styles.conditionText}>Description</Text>
+                        <View style = {styles.descContainer}>
+                            <TextInput
+                                style = {styles.descText}
+                                numberOfLines = {3}
+                                value = {values.description}
+                                onChangeText={handleChange("description")}
+                                onBlur={() => setFieldTouched("description")}
+                            />
+                            <View>
+                                {touched.description && (
+                                <Text style={styles.errorText}>{errors.description}</Text>
+                                )}
+                            </View>
+                        </View>
+                    </View>
+                    <PrimaryButton 
+                        buttonTitle = "Submit" 
+                        style = {styles.submitButton}
+                        onPress = {() => handleSubmit()}
+                    />
+                    </>
+                    )}
+
+                </Formik>
+                <View style = {styles.gap}></View>
+                
             </View>
             </ScrollView>
         </SafeAreaView>
@@ -100,7 +208,7 @@ const styles = StyleSheet.create({
     conditionText : {
         fontSize : 20,
         fontWeight : "bold",
-        color : "#fc5c65",
+        //color : "#fc5c65",
         marginBottom : 10
     },
     conditionContainer : {
@@ -141,6 +249,9 @@ const styles = StyleSheet.create({
     submitButton : {
         fontSize : 20,
         color : "#fff"
+    },
+    errorText : {
+        color : "#ff0000"
     }
 })
 
